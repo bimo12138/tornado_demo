bc5fe2f44e5cf1a78a1b6faf1161e1410c948dd3
bimo12138 <36919327+bimo12138@users.noreply.github.com>
2020-07-18 13:15:30 +0800

    change IP

--- model/db.py
+++ model/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine # 创建引擎对象的模块
 from sqlalchemy.ext.declarative import declarative_base # 基础类模块
 from sqlalchemy.orm import sessionmaker # 创建和数据库连接会话
 
-HOST = "132.232.121.87"
+HOST = "127.0.0.1"
 # MySQL 默认是3306端口
 PORT = "3306"
 DATABASE = "tornado_server"
